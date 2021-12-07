65051dc217a931705f18fe4723aee3f32ade07bc
marttak <martta.korpi@tuni.fi>
2021-12-07 13:16:02 +0200

    Added randomseed & removed game over music

--- ledmemorygame.ino
+++ ledmemorygame.ino
@@ -2,97 +2,6 @@
 #include <avr/wdt.h>
 #include <EEPROM.h>
 
-//define game over notes
-#define NOTE_B0  31
-#define NOTE_C1  33
-#define NOTE_CS1 35
-#define NOTE_D1  37
-#define NOTE_DS1 39
-#define NOTE_E1  41
-#define NOTE_F1  44
-#define NOTE_FS1 46
-#define NOTE_G1  49
-#define NOTE_GS1 52
-#define NOTE_A1  55
-#define NOTE_AS1 58
-#define NOTE_B1  62
-#define NOTE_C2  65
-#define NOTE_CS2 69
-#define NOTE_D2  73
-#define NOTE_DS2 78
-#define NOTE_E2  82
-#define NOTE_F2  87
-#define NOTE_FS2 93
-#define NOTE_G2  98
-#define NOTE_GS2 104
-#define NOTE_A2  110
-#define NOTE_AS2 117
-#define NOTE_B2  123
-#define NOTE_C3  131
-#define NOTE_CS3 139
-#define NOTE_D3  147
-#define NOTE_DS3 156
-#define NOTE_E3  165
-#define NOTE_F3  175
-#define NOTE_FS3 185
-#define NOTE_G3  196
-#define NOTE_GS3 208
-#define NOTE_A3  220
-#define NOTE_AS3 233
-#define NOTE_B3  247
-#define NOTE_C4  262
-#define NOTE_CS4 277
-#define NOTE_D4  294
-#define NOTE_DS4 311
-#define NOTE_E4  330
-#define NOTE_F4  349
-#define NOTE_FS4 370
-#define NOTE_G4  392
-#define NOTE_GS4 415
-#define NOTE_A4  440
-#define NOTE_AS4 466
-#define NOTE_B4  494
-#define NOTE_C5  523
-#define NOTE_CS5 554
-#define NOTE_D5  587
-#define NOTE_DS5 622
-#define NOTE_E5  659
-#define NOTE_F5  698
-#define NOTE_FS5 740
-#define NOTE_G5  784
-#define NOTE_GS5 831
-#define NOTE_A5  880
-#define NOTE_AS5 932
-#define NOTE_B5  988
-#define NOTE_C6  1047
-#define NOTE_CS6 1109
-#define NOTE_D6  1175
-#define NOTE_DS6 1245
-#define NOTE_E6  1319
-#define NOTE_F6  1397
-#define NOTE_FS6 1480
-#define NOTE_G6  1568
-#define NOTE_GS6 1661
-#define NOTE_A6  1760
-#define NOTE_AS6 1865
-#define NOTE_B6  1976
-#define NOTE_C7  2093
-#define NOTE_CS7 2217
-#define NOTE_D7  2349
-#define NOTE_DS7 2489
-#define NOTE_E7  2637
-#define NOTE_F7  2794
-#define NOTE_FS7 2960
-#define NOTE_G7  3136
-#define NOTE_GS7 3322
-#define NOTE_A7  3520
-#define NOTE_AS7 3729
-#define NOTE_B7  3951
-#define NOTE_C8  4186
-#define NOTE_CS8 4435
-#define NOTE_D8  4699
-#define NOTE_DS8 4978
-
 //buttons
 const int buttonPin1 = 18;
 const int buttonPin2 = 19;
@@ -109,64 +18,21 @@ const int ledPin4 = 43;
 //piezo
 const int buzzer = 53;
 
+//lcd
 LiquidCrystal_I2C lcd(0x27, 20, 4);
 
+long randNumber; //to get the next random led for the sequence
 volatile bool mistake = false;
 int run;
 volatile int score = 0; //user score
 int savedHighScore = 0; //highscore to store in eeprom
 
 volatile int ROUNDS_TO_WIN = 250; //how many rounds it takes for the game to reach the end
-                                 //first level is 0
+                                  //for the user, the first level is 1, but internally the
+                                  //first level is 0
 
 int rand_Array[249]; //array to store the sequence to remember
 int user_input[249]; //array to store the sequence user guesses
-
-//Underwolrd tempo
-int underworld_tempo[] = {
-  12, 12, 12, 12,
-  12, 12, 6,
-  3,
-  12, 12, 12, 12,
-  12, 12, 6,
-  3,
-  12, 12, 12, 12,
-  12, 12, 6,
-  3,
-  12, 12, 12, 12,
-  12, 12, 6,
-  6, 18, 18, 18,
-  6, 6,
-  6, 6,
-  6, 6,
-  18, 18, 18, 18, 18, 18,
-  10, 10, 10,
-  10, 10, 10,
-  3, 3, 3
-};
-
-//Underworld melody
-int underworld_melody[] = {
-  NOTE_C4, NOTE_C5, NOTE_A3, NOTE_A4,
-  NOTE_AS3, NOTE_AS4, 0,
-  0,
-  NOTE_C4, NOTE_C5, NOTE_A3, NOTE_A4,
-  NOTE_AS3, NOTE_AS4, 0,
-  0,
-  NOTE_F3, NOTE_F4, NOTE_D3, NOTE_D4,
-  NOTE_DS3, NOTE_DS4, 0,
-  0,
-  NOTE_F3, NOTE_F4, NOTE_D3, NOTE_D4,
-  NOTE_DS3, NOTE_DS4, 0,
-  0, NOTE_DS4, NOTE_CS4, NOTE_D4,
-  NOTE_CS4, NOTE_DS4,
-  NOTE_DS4, NOTE_GS3,
-  NOTE_G3, NOTE_CS4,
-  NOTE_C4, NOTE_FS4, NOTE_F4, NOTE_E3, NOTE_AS4, NOTE_A4,
-  NOTE_GS4, NOTE_DS4, NOTE_B3,
-  NOTE_AS3, NOTE_A3, NOTE_GS3,
-  0, 0, 0
-};
 
 void setup() {
   //initialize LED pins as outputs:
@@ -182,11 +48,17 @@ void setup() {
   pinMode(buttonPin4, INPUT_PULLUP);
   pinMode(startButton, INPUT_PULLUP);
 
+  //initialize lcd
   lcd.init();
   lcd.begin(16,4);
+  
   Serial.begin(9600);
   run = 0;
 
+  //randomseed to improve randomness
+  randomSeed(analogRead(0));
+
+  //enable watchdog timer
   wdt_enable(WDTO_8S);
 
 }
@@ -206,6 +78,7 @@ void loop() {
   lcd.print(savedHighScore);
   Serial.println("Press White Button to Start");
 
+  wdt_reset();
   //if the game is not already on, start the game when startbutton is pressed
   if (digitalRead(startButton) == LOW){
     if (run == 0){
@@ -233,8 +106,10 @@ void game(){
      do{
       wdt_reset();
       level++; //next level
-      int next_led = rand() % 4; //draws the next led for the sequence randomly
-      rand_Array[level] = next_led; //input the randomized next_led to rand_Array in the place of current level
+      randNumber = random(4);
+      int next_led = randNumber; //draws the next led for the sequence randomly
+      rand_Array[level] = next_led; //input the randomized next_led to rand_Array 
+                                    //in the place of current level
       lcd.setCursor(0,0);
       lcd.print("Current Level: ");
       lcd.print(level + 1);
@@ -254,7 +129,8 @@ void game(){
       //checks the user input
       mistake = input(level);
         
-     } while(!mistake && level < ROUNDS_TO_WIN); //do while the user doesn't make a mistake and the max number of rounds is not passed
+     } while(!mistake && level < ROUNDS_TO_WIN); //do while the user doesn't make a mistake 
+                                                 //and the max number of rounds is not passed
      
   score = level;
       if(score > savedHighScore){
